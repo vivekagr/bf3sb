db41bd01181ca6655699b185b5f57ab10026457a
vivekagr <me@vivek.im>
2013-03-07 22:39:54 +0530

    Updated GUI skeleton

--- gui.py
+++ gui.py
@@ -1,7 +1,5 @@
-from PySide import QtCore
-from PySide import QtGui
+from PySide import QtGui, QtCore
 from bf3 import BF3Server
-
 import sys
 
 
@@ -10,25 +8,48 @@ class MainWindow(QtGui.QDialog):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setWindowTitle('BF3 Server Pinger')
+        self.setWindowFlags(QtGui.QStyle.SP_TitleBarMinButton)
         # Main Vertical Box Layout
         vbox = QtGui.QVBoxLayout()
 
         # Maps List
-        map_check_box, map_group_box = self.make_layout(col=2, label_list=BF3Server.map_code.values(),
-                                                        group_box_label='MAPS')
+        map_check_box, map_widget = self.make_layout(2, BF3Server.map_code.values(), 'MAPS')
 
         # Mode List
-        mode_check_box, mode_group_box = self.make_layout(col=2, label_list=BF3Server.game_mode.values(),
-                                                          group_box_label='MODE')
+        mode_check_box, mode_widget = self.make_layout(2, BF3Server.game_mode.values(), 'MODE')
 
         # Game Size List
+        game_size_check_box, game_size_widget = self.make_layout(6, BF3Server.game_size.values(), 'GAME SIZE')
 
+        # Free Slots List
+        free_slots_check_box, free_slots_widget = self.make_layout(5, BF3Server.free_slots.values(), 'FREE SLOTS')
 
-        temp_grid = QtGui.QGridLayout()
-        temp_grid.addWidget(mode_group_box, 0, 0)
-        temp_grid.addWidget(map_group_box, 0, 1, 2)
+        # Preset List
+        preset_check_box, preset_widget = self.make_layout(3, BF3Server.preset.values(), 'PRESET')
 
-        self.setLayout(temp_grid)
+        browse_button = QtGui.QPushButton('Browse')
+        default_button = QtGui.QPushButton('Default')
+        button_hbox = QtGui.QHBoxLayout()
+        button_hbox.addWidget(default_button)
+        button_hbox.addStretch(1)
+        button_hbox.addWidget(browse_button)
+
+        hbox = QtGui.QHBoxLayout()
+
+        vbox_inner = QtGui.QVBoxLayout()
+        vbox_inner.addWidget(mode_widget)
+        vbox_inner.addWidget(game_size_widget)
+        vbox_inner.addWidget(free_slots_widget)
+        vbox_inner.addWidget(preset_widget)
+
+        hbox.addLayout(vbox_inner)
+        hbox.addWidget(map_widget)
+
+        vbox.addLayout(hbox)
+        vbox.addStretch(0)
+        vbox.addLayout(button_hbox)
+
+        self.setLayout(vbox)
 
     def make_layout(self, col, label_list, group_box_label):
         check_box_list = []
